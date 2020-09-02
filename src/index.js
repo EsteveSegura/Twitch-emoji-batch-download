7428fc81d829697c490c1a16e356a2a17e2c9916
estevesegura <estevesegura.gir@gmail.com>
2020-09-02 19:33:34 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const https = require('https')
+const fs = require('fs')
+//https://static-cdn.jtvnw.net/emoticons/v1/303436380/3.0
+//400000
+
+function downloadSingleEmoji(url) {
+    const fileName = url.split('/v1/')[1].split("/3.0")[0]
+    const file = fs.createWriteStream(`./newEmojis/${fileName}.png`)
+    const request = https.get(url, (response) => {
+        response.pipe(file)
+    })
+}
+
+async function sleep(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        },ms);
+    })
+}
+
+(async () => {
+    for (let i = 300002465; i < 303500000; i++){
+        console.log(`Descargando la imagen : ${i} ...`)
+        await sleep(250)
+        downloadSingleEmoji(`https://static-cdn.jtvnw.net/emoticons/v1/${i}/3.0`)
+    }
+})()
